ad996270c89986348b88c1596dd741df295426d3
Kimsanova <synak2013@gmail.com>
2020-04-27 02:14:14 +0600

    todo list with filters

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid dolores nisi nostrum tempore? Dolore error minima nobis odio quas!</p>
+
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
